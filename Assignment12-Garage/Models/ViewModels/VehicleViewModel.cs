83b62fe49a9a7f9941e9fb3d22b21adddb68fc35
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-04-17 16:21:46 +0200

    Added class ViewModel for Vehicle

---
+++ Assignment12-Garage/Models/ViewModels/VehicleViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment12_Garage.Models.ViewModels
+{
+    public class VehicleViewModel
+    {
+        public VehicleType VehicleType { get; set; }
+        public string RegNumber { get; set; }
+        public DateTime ArrivalDate { get; set; }
+    }
+}
