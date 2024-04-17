61260c7ed55bd7055b9c32e00653d9b35a700cb0
Victor S <victor.skarerverket@gmail.com>
2024-04-17 12:15:57 +0200

    Added OnModelCreation seed data in GarageContext.cs

--- Assignment12-Garage/Data/Assignment12_GarageContext.cs
+++ Assignment12-Garage/Data/Assignment12_GarageContext.cs
@@ -9,11 +9,24 @@ namespace Assignment12_Garage.Data
 {
     public class Assignment12_GarageContext : DbContext
     {
-        public Assignment12_GarageContext (DbContextOptions<Assignment12_GarageContext> options)
+        public Assignment12_GarageContext(DbContextOptions<Assignment12_GarageContext> options)
             : base(options)
         {
         }
 
         public DbSet<Assignment12_Garage.Models.Vehicle> Vehicle { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Vehicle>().HasData(
+                new Vehicle { Id = 1, VehicleType = VehicleType.Boat, RegNumber = "123ABC", Color = "Red", Brand = "Stenaline", VehicleModel = "Ferry", NrOfWheels = 0, ArrivalDate = DateTime.Parse("1993-01-05") },
+                new Vehicle { Id = 2, VehicleType = VehicleType.Boat, RegNumber = "ABC123", Color = "Blue", Brand = "Stenaline", VehicleModel = "JokeBoat", NrOfWheels = 0, ArrivalDate = DateTime.Parse("2023-01-05") },
+                new Vehicle { Id = 3, VehicleType = VehicleType.Car, RegNumber = "BRUM", Color = "Yellow", Brand = "Aston Martin", VehicleModel = "BRUM", NrOfWheels = 4, ArrivalDate = DateTime.Parse("2003-01-05") }
+                );
+
+
+        }
     }
 }
