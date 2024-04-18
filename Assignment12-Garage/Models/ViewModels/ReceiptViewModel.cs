554854beb4ae510eecd5f4092cf0666ce489ccca
Sillysally <92034603+SallyResch@users.noreply.github.com>
2024-04-18 08:35:15 +0200

    Added ReceiptViewModel and changed delete text in Index.cshtml to checkout

---
+++ Assignment12-Garage/Models/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment12_Garage.Models.ViewModels
+{
+    public class ReceiptViewModel
+    {
+        public string RegNumber { get; set; }
+        public DateTime ArrivalDate { get; set; }
+        public DateTime CheckoutDate { get; set; }
+        public int TotalParkingHours { get; set; }
+        public double Price { get; set; }
+
+       /* public void CalculateTotalParkingHours()
+        {
+            TimeSpan parkingDuration = CheckoutDate - ArrivalDate;
+            TotalParkingHours = (int)parkingDuration.TotalHours;
+        }
+
+        public void CalculatePrice()
+        {
+            const double HourlyRate = 50;
+            Price = TotalParkingHours * HourlyRate;
+        }*/
+    }
+}
